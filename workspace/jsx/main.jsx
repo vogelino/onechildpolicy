a60373da79b87e2d890ee14fe40f3c2ded5596cd
Lucas Vogel <contact@vogelino.ch>
2015-06-16 17:24:39 +0200

    Added basic files (jsx, stylus, html, etc.)

---
+++ workspace/jsx/main.jsx
@@ -0,0 +1,17 @@
+/** @jsx React.DOM */
+
+/*global require*/
+require.config({
+	shim: {
+	},
+	paths: {
+		jquery: '../bower_components/jquery/jquery',
+		underscore: '../bower_components/underscore/underscore',
+		text: '../bower_components/requirejs-text/text',
+		json: '../bower_components/requirejs-plugins/src/json'
+	}
+});
+
+require([
+], function() {
+});
