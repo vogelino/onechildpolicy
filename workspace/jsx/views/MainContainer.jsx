b233c4b3084ff5bfe0e30f12e560fa506359579a
vogelino <contact@vogelino.ch>
2015-06-21 18:19:40 +0200

    Added more data

--- workspace/jsx/views/MainContainer.jsx
+++ workspace/jsx/views/MainContainer.jsx
@@ -2,9 +2,22 @@
 
 define([
 	'views/charts/ChartsContainer/ChartsContainer',
+
 	'json!../../../data/demographyData/urbanVsRural.json',
-	'json!../../../data/demographyData/populationAge.json'
-], function(ChartsContainer, urbanVsRural, populationAge) {
+	'json!../../../data/demographyData/populationAge.json',
+
+	'json!../../../data/birthAndChildhood/fertilityRate.json',
+	'json!../../../data/birthAndChildhood/birthRate.json',
+	'json!../../../data/birthAndChildhood/lifeExpectancyAtBirth.json'
+], function(
+	ChartsContainer,
+
+	urbanVsRural,
+	populationAge,
+
+	fertilityRate,
+	birthRate,
+	lifeExpectancyAtBirth) {
 	'use strict';
 
 	var MainContainer = function() {
@@ -12,31 +25,71 @@ define([
 		var my = {};
 
 		that.render = function() {
-			var chartContainers = [
-				{
-					title: 'Absolute chinese population',
-					id: 'chart1',
-					key: 1,
-					data: urbanVsRural,
-					type: 'area-step'
-				},
-				{
-					title: 'Chinese\'s population by age groups',
-					id: 'chart2',
-					key: 2,
-					data: populationAge,
-					type: 'area-spline'
-				}
-			];
+			var chartContainers = my.getChartContainers();
 
 			return (
 				<div id='main-container'>
-					<h1 className='main-title'>
-						{'The evolution of the chinese demography since the 60\'s'}
-					</h1>
-					<ChartsContainer charts={chartContainers} />
+					<div className='block-header'>
+						<h1>
+							{'Chinese\'s demography evolution since the 60\'s'}
+						</h1>
+						<p>
+							{'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, quia, autem. Obcaecati odit ipsa dolores enim deleniti officiis distinctio velit, aliquam provident deserunt, magnam dolorum! Dolore, labore minima? Pariatur, adipisci.'}
+						</p>
+					</div>
+					<ChartsContainer charts={chartContainers.demography} />
+					<div className='block-header'>
+						<h1>
+							{'Chinese\'s births and fertility evolution since the 60\'s'}
+						</h1>
+					</div>
+					<ChartsContainer charts={chartContainers.birthAndChildhood} />
 				</div>
 			);
+		};
+
+		my.getChartContainers = function() {
+			return {
+				demography: [
+					{
+						title: 'Chinese\'s absolute population',
+						id: 'chart1',
+						key: 1,
+						data: urbanVsRural,
+						type: 'area-step'
+					},
+					{
+						title: 'Chinese\'s population by age groups',
+						id: 'chart2',
+						key: 2,
+						data: populationAge,
+						type: 'area-spline'
+					}
+				],
+				birthAndChildhood: [
+					{
+						title: 'Chinese\'s fertility rate',
+						id: 'chart3',
+						key: 3,
+						data: fertilityRate,
+						type: 'area-step'
+					},
+					{
+						title: 'Chinese\'s crude birth rate',
+						id: 'chart4',
+						key: 4,
+						data: birthRate,
+						type: 'line'
+					},
+					{
+						title: 'Chinese\'s life expectancy at birth',
+						id: 'chart5',
+						key: 5,
+						data: lifeExpectancyAtBirth,
+						type: 'line'
+					}
+				]
+			};
 		};
 
 		return React.createClass(that);
